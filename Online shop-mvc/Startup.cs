0592f07c2fc929e6c90e16ade316e06140f99112
Ahasanul Kabir <mdahasanulkabir6@gmail.com>
2021-03-04 23:27:53 +0600

    bootstrep update

---
+++ Online shop-mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Online_shop_mvc.Startup))]
+namespace Online_shop_mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
