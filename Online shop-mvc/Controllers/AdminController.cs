c0536e04b09b09f9d6bdbc07d81312a594abba09
Ahasanul Kabir <mdahasanulkabir6@gmail.com>
2021-03-19 23:41:48 +0600

    Admin dashbord added

---
+++ Online shop-mvc/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using Online_shop_mvc.DAL;
+using Online_shop_mvc.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Online_shop_mvc.Controllers
+{
+    public class AdminController:Controller
+    {
+        public GenericUnitOfWork _unitOfWork = new GenericUnitOfWork();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Categories()
+        {
+            List<Tbl_Category> allCategories = _unitOfWork.GetRepositoryInstance<Tbl_Category>().GetAllRecordsIQueryable().Where(i => i.IsDeleted == false).ToList();
+
+            return View(allCategories);
+        }
+    }
+}
